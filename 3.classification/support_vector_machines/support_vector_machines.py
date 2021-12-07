c3a8e20483ea64e4128778e9b5b51c0d4dae51c3
Emil <emilpradeep@gmail.com>
2021-12-07 16:28:04 +0530

    reverted changes in support vector machines

--- 3.classification/support_vector_machines/support_vector_machines.py
+++ 3.classification/support_vector_machines/support_vector_machines.py
@@ -9,8 +9,8 @@ dataset = pd.read_csv('3.classification/support_vector_machines/Social_Network_A
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-sc = StandardScaler()
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.fit_transform(X_test)
 
