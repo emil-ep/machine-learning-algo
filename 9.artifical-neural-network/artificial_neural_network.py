f6c9898c70b8b1444dda8e920db53ee5ac222376
Emil <emilpradeep@gmail.com>
2021-12-23 23:50:42 +0530

    working with artifical neural network

--- 9.artifical-neural-network/artificial_neural_network.py
+++ 9.artifical-neural-network/artificial_neural_network.py
@@ -2,4 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+from
 
+print(tf.__version__)
+
+dataset = pd.read_csv('9.artifical-neural-network/Churn_Modelling.csv')
+X = dataset.iloc[:, 3:-1].values
+y = dataset.iloc[:, -1].values
