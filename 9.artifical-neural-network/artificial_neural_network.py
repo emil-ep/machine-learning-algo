1835a4ae3679fcb91fbfca07714ba78d62d37a0b
Emil <emilpradeep@gmail.com>
2022-01-03 19:32:26 +0530

    working on ANN

--- 9.artifical-neural-network/artificial_neural_network.py
+++ 9.artifical-neural-network/artificial_neural_network.py
@@ -2,10 +2,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-from
+from sklearn.preprocessing import LabelEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
 
 print(tf.__version__)
 
 dataset = pd.read_csv('9.artifical-neural-network/Churn_Modelling.csv')
 X = dataset.iloc[:, 3:-1].values
 y = dataset.iloc[:, -1].values
+
+print(X)
+#Label encoding of the gender column
+le = LabelEncoder()
+X[:, 2] = le.fit_transform(X[:, 2])
+
+print(X)
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+print(X)
+
+#splitting the data into train and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+#FEATURE SCALING IS MANDATORY FOR DEEP LEARNING
+#IT IS MANDATORY TO APPLY FEATURE SCALING FOR ALL THE FEATURES
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.fit_transform(X_test)
